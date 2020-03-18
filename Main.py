cf8220c86dacc332492ddb4179bc3f2526ba4be8
brorie3 <60199433+brorie3@users.noreply.github.com>
2020-03-18 17:51:22 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,17 +3,24 @@ from Invoice import Invoice
 products = {}
 total_amount = 0
 repeat = ''
+# new variable to keep track of amount of items
+# item_count = 0
 
 while True:
-    product = input("What is your product : ")
+    # product = input("What is your product : ")
+    product = Invoice().inputName("What is your product: ")
     unit_price = Invoice().inputNumber("Please enter unit price : ")
     qnt = Invoice().inputNumber("Please enter quantity of product : ")
     discount = Invoice().inputNumber("Discount percent (%) : ")
     repeat = Invoice().inputAnswer("Another product? (y,n) : ")
-    result = Invoice().addProduct(qnt, unit_price, discount)
+    result = Invoice().addProduct(qnt, unit_price, discount, product)
     products[product] = result
     if repeat == "n":
         break;
 
 total_amount = Invoice().totalPurePrice(products)
 print("Your total pure price is: ", total_amount)
+
+# print the total amount of items
+names = Invoice().allNames(products)
+print("Types of items purchased: ", names)

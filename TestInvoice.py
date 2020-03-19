9ca96009234446c54325edae805a5219890a0f9a
sabyrd39 <48448793+sabyrd39@users.noreply.github.com>
2020-03-18 21:38:28 -0400

    Update TestInvoice.py

--- TestInvoice.py
+++ TestInvoice.py
@@ -4,8 +4,8 @@ from Invoice import Invoice
 
 @pytest.fixture()
 def products():
-    products = {'Pen': {'qnt': 10, 'unit_price': 3.75, 'discount': 5},
-                'Notebook': {'qnt': 5, 'unit_price': 7.5, 'discount': 10}}
+    products = {'Pen': {'qnt': 10, 'unit_price': 3.75, 'discount': 5, 'name': "Pen"},
+                'Notebook': {'qnt': 5, 'unit_price': 7.5, 'discount': 10, 'name': "Notebook"}}
     return products
 
 
@@ -13,7 +13,6 @@ def products():
 def invoice():
     invoice = Invoice()
     return invoice
-
 
 def test_CanCalculateTotalImpurePrice(invoice, products):
     invoice.totalImpurePrice(products)
@@ -28,3 +27,7 @@ def test_CanCalculateTotalDiscount(invoice, products):
 def test_CanCalculateTotalPurePrice(invoice, products):
     invoice.totalPurePrice(products)
     assert invoice.totalPurePrice(products) == 69.38
+
+def test_allNames(invoice, products):
+    output = invoice.allNames(products)
+    assert output == "Pen | Notebook"
